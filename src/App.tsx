648a93e5af4bb105ff5edc03510ed5c2a7a926fd
mohamed <mohamedyounis150@gmail.com>
2024-10-21 12:42:02 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="background">
+    <div className="circle-container">
+      <div className="circle component1">Component1</div>
+      <div className="circle component2">Component2</div>
+      <div className="circle component3">Component3</div>
+      <div className="circle component4">Component4</div>
+    </div>
+  </div>
+  );
+};
+
+export default App;
